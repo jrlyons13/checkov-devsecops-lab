eabd0ca5d7f20c0674ab3319ec20a7c58eec1818
Jmaes <jrlyons13@gmail.com>
2026-04-26 20:47:52 -0400

    Enable S3 KMS encryption

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,22 @@ resource "aws_s3_bucket_versioning" "bad_bucket_versioning" {
     status = "Enabled"
   }
 }
+resource "aws_kms_key" "s3_key" {
+  description             = "KMS key for Checkov DevSecOps lab S3 bucket"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+}
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "bad_bucket_encryption" {
+  bucket = aws_s3_bucket.bad_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
 resource "aws_s3_bucket_public_access_block" "bad_bucket_public_access" {
   bucket = aws_s3_bucket.bad_bucket.id
 
